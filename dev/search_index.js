0c74a643e40427c1f0999639694a188bbce030c2
Documenter.jl <documenter@juliadocs.github.io>
2024-12-28 06:40:53 +0000

    build based on d365d0a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VOPLS","category":"page"},{"location":"#VOPLS","page":"Home","title":"VOPLS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VOPLS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VOPLS]","category":"page"}]
+}
